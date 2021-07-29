3fa095f0aaf28e0ff0fd10240330bc7569645c3e
umer <umer229@gmail.com>
2021-07-29 10:01:43 +0200

    design basic done

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,18 @@
+import planetData from "../../data.js"
+
+
+
 const btnHamburger = document.querySelector('#btnHamburger');
 const sideDrawer = document.querySelector('.side-drawer');
-
+const header = document.querySelector('.header');
+console.log(12, planetData);
 btnHamburger.addEventListener('click', () => {
-  console.log()
+  console.log(77)
   if(sideDrawer.classList.contains('side-drawer__is-opened')) {
     sideDrawer.classList.remove('side-drawer__is-opened');
+    header.classList.remove('open');
   } else {
     sideDrawer.classList.add('side-drawer__is-opened');
+    header.classList.add('open');
   }
 })
